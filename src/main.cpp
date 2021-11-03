552ebda1ce16ee2d527248d6745f7b93c033ab3d
Julia Rodrigues <j.pelayo.rodrigues@gmail.com>
2021-11-03 19:54:45 -0300

    camera se move sem precisar apertar o botão do mouse

--- src/main.cpp
+++ src/main.cpp
@@ -1107,7 +1107,7 @@ void CursorPosCallback(GLFWwindow* window, double xpos, double ypos)
     // parâmetros que definem a posição da câmera dentro da cena virtual.
     // Assim, temos que o usuário consegue controlar a câmera.
 
-    if (g_LeftMouseButtonPressed)
+    if (true)
     {
         // Deslocamento do cursor do mouse em x e y de coordenadas de tela!
         float dx = xpos - g_LastCursorPosX;
